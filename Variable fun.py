ed80eb5fca839ad64436324ac699522097726e16
EndyCode-os <endypongnon4@gmail.com>
2025-01-23 00:09:46 -0500

    Add files via upload

---
+++ Variable fun.py
@@ -0,0 +1,31 @@
+value1 = int(input("Enter the first value:\n"))
+value2= int(input("Enter the second value:\n"))
+
+multiplication =   value1 * value2
+print("The answer to your query is {}\n".format(multiplication))
+
+value1 = int(input("Enter the first value:\n"))
+value2= int(input("Enter the second value:\n"))
+
+division =   value1 / value2
+print("The answer to your query is {}".format(division))
+
+remainder  =   value1 // value2
+print("The answer to your query is {}".format(remainder))
+
+modulo =   value1 % value2
+print("The answer to your query is {}\n".format(modulo))
+
+value1 = int(input("Enter the first value:\n"))
+value2= int(input("Enter the second value:\n"))
+
+addition =   value1 + value2
+print("The answer to your query is {}\n".format(addition))
+
+value1 = int(input("Enter the first value:\n"))
+value2= int(input("Enter the second value:\n"))
+
+subtraction =   value1 - value2
+print("The answer to your query is {}\n".format(subtraction))
+
+print("Endy Pongnon")
